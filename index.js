fb84882ed8f29d751fa96a918ea0c95641dcc2cc
MarcisUrbans <urbans.marcis@gmail.com>
2023-12-14 12:40:55 +0200

    checking redeploy setup

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+app.get("/", (req, res) => {
+  res.send({ testing: "wasabiiiiiiiii" });
 });
 
 const PORT = process.env.PORT || 5000;
